4563ef775bb4b8b49f76c401e7855eaa718dfabd
ednync <ednync@gmail.com>
2021-07-16 13:13:34 +0700

    deply third

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
@@ -15,11 +15,13 @@ function App() {
     <Header />
     <main>
     <Container>
+    <Switch>
      <Route path='/neon' component={TagsNeon} />
      <Route path='/pure' component={TagsPure} />
      <Route path='/page' component={PageBreakTest} />
      <Route path='/line' component={LineSheet} />
      <Route path='/' component={Home} exact />
+     </Switch>
      </Container>
      </main>
     </Router>
